9f537805d6dabca05fee31f6b57d8bf33ff7bd1e
Marcos Romero Lamas <mromerol@lxplus.cern.ch>
2021-08-08 12:55:37 +0200

    these to files are useless

---
+++ lua/general/theme.lua
@@ -0,0 +1,43 @@
+-- nvim.opt["termguicolors"] = true, -- set term gui colors (most terminals support this)
+
+-- vim.g.colors_style = "deep ocean"
+require("assegonia")
+-- vim.cmd("colorscheme " .. "assegonia")
+-- nvim.colorscheme = "gruvbox-material"
+-- nvim.colorscheme = "github-nvim-theme"
+-- nvim.colorscheme = "nord"
+-- nvim.colorscheme = "doom-one"
+-- require('nord').set()
+
+-- vim.o.background = "dark"
+-- Character for vertical split pane
+-- vim.o.fillchars = "vert:│"
+
+-- Gruvbox {{{
+
+-- vim.g.gruvbox_material_background = 'soft'
+-- vim.g.gruvbox_material_palette = 'original'
+-- vim.g.gruvbox_italic = '1'
+-- vim.g.gruvbox_invert_selection = '1'
+-- vim.cmd("colorscheme " .. nvim.colorscheme)
+
+-- nvim.colorscheme = "gruvbox"
+-- vim.g.gruvbox_contrast_dark = 'hard'
+
+-- }}}
+
+
+-- Material {{{
+
+-- vim.g.material_style = "darker"
+-- vim.g.material_italic_comments = true
+-- vim.g.material_terminal_italics = "1"
+-- require('material').set()
+
+-- }}}
+
+
+
+
+
+-- dfsdf
