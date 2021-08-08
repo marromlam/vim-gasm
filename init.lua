6323e525a6d9b80abec7785ba86b2b8c4eaaf98f
Marcos Romero Lamas <mromerol@lxplus.cern.ch>
2021-08-08 06:02:55 +0200

    sort and clean

--- init.lua
+++ init.lua
@@ -1,58 +1,155 @@
 vim.cmd [[
-  set packpath-=~/.config/nvim
-  set packpath-=~/.config/nvim/after
-  set packpath-=~/.local/share/nvim/site
-  set packpath^=~/.local/share/lunarvim/site
-  set packpath^=~/.config/lvim
+  set packpath^=~/.config/nvim/autoload
+  set packpath^=~/.config/nvim
 
-  set runtimepath-=~/.config/nvim
-  set runtimepath-=~/.config/nvim/after
-  set runtimepath+=~/.config/lvim
-  set runtimepath^=~/.local/share/lunarvim/lvim/after
+  set runtimepath+=~/.config/nvim
+  set runtimepath^=~/.config/nvim/after
 ]]
 -- vim.opt.rtp:append() instead of vim.cmd ?
 
-local function file_exists(name)
-  local f = io.open(name, "r")
-  if f ~= nil then
-    io.close(f)
-    return true
-  else
-    return false
-  end
-end
 
-local lvim_path = os.getenv "HOME" .. "/.config/lvim/"
-USER_CONFIG_PATH = lvim_path .. "config.lua"
-local config_exist = file_exists(USER_CONFIG_PATH)
-if not config_exist then
-  USER_CONFIG_PATH = lvim_path .. "lv-config.lua"
-  print "Rename ~/.config/lvim/lv-config.lua to config.lua"
-end
+local nvim_path = os.getenv "HOME" .. "/.config/nvim/"
+USER_CONFIG_PATH = nvim_path .. "config.lua"
+
 
 require "default-config"
-local autocmds = require "core.autocmds"
-require("settings").load_options()
+require "all-packs"
+
+nvim.colorscheme = "assegonia"
+-- vim.g.colors_style = "oceanic"
+-- vim.g.colors_lighter_contrast = true
+
+local autocmds = require "packconf.autocmds"
+require("general.settings").load_options()
 
 local status_ok, error = pcall(vim.cmd, "luafile " .. USER_CONFIG_PATH)
 if not status_ok then
-  print("something is wrong with your " .. USER_CONFIG_PATH)
+  print("Something is wrong with your " .. USER_CONFIG_PATH)
   print(error)
 end
-require("settings").load_commands()
-autocmds.define_augroups(lvim.autocommands)
 
+require("general.settings").load_commands()
+autocmds.define_augroups(nvim.autocommands)
+
+
+-- Load plugings
 local plugins = require "plugins"
-local plugin_loader = require("plugin-loader").init()
-plugin_loader:load { plugins, lvim.plugins }
-vim.g.colors_name = lvim.colorscheme -- Colorscheme must get called after plugins are loaded or it will break new installs.
-vim.cmd("colorscheme " .. lvim.colorscheme)
+local plugin_loader = require("all-packs").init()
+plugin_loader:load { plugins, nvim.plugins }
 
+
+-- TO BE MOVED {{{
+
+vim.g.netrw_bufsettings = "noma nomod nonu nowrap ro nobuflisted"
+
+vim.g.nvim_tree_disable_netrw = 0 -- 1 by default, disables netrw
+vim.g.nvim_tree_hijack_netrw = 0 -- 1 b
+
+-- Set barbar's options {{{
+
+vim.g.bufferline = {
+  -- Enable/disable animations
+  animation = true,
+
+  -- Enable/disable auto-hiding the tab bar when there is a single buffer
+  auto_hide = false,
+
+  -- Enable/disable current/total tabpages indicator (top right corner)
+  tabpages = true,
+
+  -- Enable/disable close button
+  closable = true,
+
+  -- Enables/disable clickable tabs
+  --  - left-click: go to buffer
+  --  - middle-click: delete buffer
+  clickable = true,
+
+  -- Excludes buffers from the tabline
+  -- exclude_ft = ['javascript'],
+  -- exclude_name = ['package.json'],
+
+  -- Enable/disable icons
+  -- if set to 'numbers', will show buffer index in the tabline
+  -- if set to 'both', will show buffer index and icons in the tabline
+  icons = true,
+
+  -- If set, the icon color will follow its corresponding buffer
+  -- highlight group. By default, the Buffer*Icon group is linked to the
+  -- Buffer* group (see Highlighting below). Otherwise, it will take its
+  -- default value as defined by devicons.
+  icon_custom_colors = false,
+
+  -- Configure icons on the bufferline.
+  icon_separator_active = '',
+  icon_separator_inactive = '',
+  icon_close_tab = '',
+  icon_close_tab_modified = '●',
+  icon_pinned = '車',
+
+  -- Sets the maximum padding width with which to surround each tab
+  maximum_padding = 2,
+
+  -- Sets the maximum buffer name length.
+  maximum_length = 30,
+
+  -- If set, the letters for each buffer in buffer-pick mode will be
+  -- assigned based on their name. Otherwise or in case all letters are
+  -- already assigned, the behavior is to assign letters in order of
+  -- usability (see order below)
+  semantic_letters = true,
+
+  -- New buffer letters are assigned in this order. This order is
+  -- optimal for the qwerty keyboard layout but might need adjustement
+  -- for other layouts.
+  letters = 'asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP',
+
+  -- Sets the name of unnamed buffers. By default format is "[Buffer X]"
+  -- where X is the buffer number. But only a static string is accepted here.
+  no_name_title = nil,
+}
+
+-- }}}
+
+-- {{{
+
+
+vim.cmd [[
+xmap <S-CR> <Plug>SlimeRegionSend<CR>
+" let fg_target = 'red'
+"
+" let fg_current  = s:fg(['Normal'], '#efefef')
+" let fg_visible  = s:fg(['TabLineSel'], '#efefef')
+" let fg_inactive = s:fg(['TabLineFill'], '#888888')
+"
+" let fg_modified  = s:fg(['WarningMsg'], '#E5AB0E')
+" let fg_special  = s:fg(['Special'], '#599eff')
+" let fg_subtle  = s:fg(['NonText', 'Comment'], '#555555')
+"
+" let bg_current  = s:bg(['Normal'], '#000000')
+" let bg_visible  = s:bg(['TabLineSel', 'Normal'], '#000000')
+" let bg_inactive = s:bg(['TabLineFill', 'StatusLine'], '#000000')
+]]
+-- set fillchars=fold:\ ,vert:\│,eob:\ ,msgsep:‾
+
+-- }}}
+
+-- }}}
+
+
+-- Set theme
+-- require "general.theme"
+require("appearance")
+
+
+-- Some utils
 local utils = require "utils"
 utils.toggle_autoformat()
-local commands = require "core.commands"
+local commands = require "packconf.commands"
 commands.load(commands.defaults)
 
+
+-- LSP
 require("lsp").config()
 
 local null_status_ok, null_ls = pcall(require, "null-ls")
@@ -64,15 +161,13 @@ end
 local lsp_settings_status_ok, lsp_settings = pcall(require, "nlspsettings")
 if lsp_settings_status_ok then
   lsp_settings.setup {
-    config_home = os.getenv "HOME" .. "/.config/lvim/lsp-settings",
+    config_home = os.getenv "HOME" .. "/.config/nvim/lsp-settings",
   }
 end
 
-require("keymappings").setup()
 
--- TODO: these guys need to be in language files
--- if lvim.lang.emmet.active then
---   require "lsp.emmet-ls"
--- end
--- if lvim.lang.tailwindcss.active then
---   require "lsp.tailwind
+-- Key mappings
+require("keys.mappings").setup()
+
+
+-- vim:foldmethod=marker
