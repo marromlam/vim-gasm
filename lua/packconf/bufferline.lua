50e8aa31f319df13fe709033d9a1a141a3655ec7
Marcos Romero Lamas <mromerol@lxplus.cern.ch>
2021-08-08 12:57:59 +0200

    barbar config to lua

---
+++ lua/packconf/bufferline.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+M.setup = function()
+  -- Set barbar's options
+end
+
+-- nvim.builtin.bufferline = {
+--   keymap = {
+--     normal_mode = {
+--       ["<S-l>"] = ":BufferNext<CR>",
+--       ["<S-h>"] = ":BufferPrevious<CR>",
+--     },
+--   },
+-- }
+
+-- local keymap = require "keys.mappings"
+-- keymap.append_to_defaults(nvim.builtin.bufferline.keymap)
+
+return M
