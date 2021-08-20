c708aa890081c6e40f923fb547f25e4f5537a0e6
Marcos Romero Lamas <marromlam@gmail.com>
2021-08-20 18:15:49 +0200

    add appearance config files

---
+++ lua/appearance/which-key.lua
@@ -0,0 +1,9 @@
+local Whichkey = {
+  WhichKey = { fg = C.purple },
+  WhichKeySeparator = { fg = C.green },
+  WhichKeyGroup = { fg = C.blue },
+  WhichKeyDesc = { fg = C.light_blue },
+  WhichKeyFloat = { fg = C.light_blue, bg = C.bg_alt },
+}
+
+return Whichkey
