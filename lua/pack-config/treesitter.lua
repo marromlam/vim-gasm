5d09a2d4ad887febe2460a987d921036a6c1ee51
Marcos Romero Lamas <marromlam@gmail.com>
2021-08-20 16:06:54 +0200

    add config with python cpp bash and lua

---
+++ lua/pack-config/treesitter.lua
@@ -0,0 +1,17 @@
+local present, ts_config = pcall(require, "nvim-treesitter.configs")
+if not present then
+   return
+end
+
+ts_config.setup {
+   ensure_installed = {
+      "bash",
+      "lua",
+      "cpp", 
+      "python"
+   },
+   highlight = {
+      enable = true,
+      use_languagetree = true,
+   },
+}
