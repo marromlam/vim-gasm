f9e603c92af3c9490022bec20fc5fccc602aeb85
Marcos Romero Lamas <marromlam@gmail.com>
2021-09-02 10:39:41 +0200

    fix blankline colors

--- lua/packer_list.lua
+++ lua/packer_list.lua
@@ -230,12 +230,36 @@ return packer.startup(function()
   }
 
   packer.use {
-      "lukas-reineke/indent-blankline.nvim",
-      disable = false,
-      event = "BufRead",
-      setup = function()
-          require("pack-config.blankline").config()
-      end
+    "lukas-reineke/indent-blankline.nvim",
+    disable = false,
+    event = "BufRead",
+    config = function()
+      require("pack-config.blankline").config()
+      vim.opt.termguicolors = true
+      -- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 blend=nocombine]]
+      -- vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B blend=nocombine]]
+      -- vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 blend=nocombine]]
+      -- vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 blend=nocombine]]
+      -- vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF blend=nocombine]]
+      -- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD blend=nocombine]]
+      -- 
+      -- vim.opt.listchars = {
+      --     space = "⋅",
+      --     eol = "↴",
+      -- }
+      -- 
+      -- require("indent_blankline").setup {
+      --     space_char_blankline = " ",
+      --     char_highlight_list = {
+      --         "IndentBlanklineIndent1",
+      --         "IndentBlanklineIndent2",
+      --         "IndentBlanklineIndent3",
+      --         "IndentBlanklineIndent4",
+      --         "IndentBlanklineIndent5",
+      --         "IndentBlanklineIndent6",
+      --     },
+      -- }
+    end
   }
 
   -- }}}
@@ -432,6 +456,7 @@ return packer.startup(function()
     "kyazdani42/nvim-tree.lua",
     disable = false,
     cmd = "NvimTreeToggle",
+    event = "BufWinEnter",
     config = function()
       require("pack-config.nvimtree").config()
     end,
@@ -643,6 +668,7 @@ return packer.startup(function()
   packer.use {
     "terrortylor/nvim-comment",
     disable = false,
+    event = "BufWinEnter",
     cmd = "CommentToggle",
     config = function()
       require("pack-config.comment").config()
