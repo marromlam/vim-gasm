a72a4b1b0380e68fcbf157c85d82b622ce4704c1
Marcos Romero Lamas <marromlam@gmail.com>
2021-10-20 19:16:22 +0200

    fix tab numbers

--- lua/pack-config/bufferline.lua
+++ lua/pack-config/bufferline.lua
@@ -1,10 +1,3 @@
--- local global_theme = "themes/" .. vim.g.nvchad_theme
--- local colors = require(global_theme)
-
-local _, colors = pcall(require,  "appearance.palettes." .. "assegonia")
-local _, _colors = pcall(require,  "appearance.palettes." .. vim.g.colorscheme)
-for k,v in pairs(_colors) do colors[k] = v end
-
 local present, bufferline = pcall(require, "bufferline")
 if not present then
     return
@@ -16,7 +9,7 @@ bufferline.setup {
         buffer_close_icon = "",
         modified_icon = "",
         -- modified_icon = "●",
-        numbers = "buffer_id",
+        numbers = "ordinal",
         close_icon = "",
         show_close_icon = false,
         left_trunc_marker = "",
@@ -33,32 +26,32 @@ bufferline.setup {
         --mappings = true,
         always_show_bufferline = true
     },
-    highlights = {
-      -- fill is the color of the bufferline (set to be the same as galaxyline)
-      fill = { guifg = colors.dark, guibg = colors.line_bg },
-      -- this is the default skin of a buffer tab
-      background = { guifg = colors.accent, guibg = colors.dark },
-      -- buffers
-      buffer_visible = { guifg = colors.accent, guibg = colors.bg },
-      buffer_selected = { guifg = colors.white, guibg = colors.bg, gui = "bold" },
-      -- tabs
-      tab = { guifg = colors.accent, guibg = colors.red },
-      tab_selected = { guifg = colors.accent, guibg = colors.bg },
-      -- this is a close buttom at right of bufferline (disabled)
-      tab_close = { guifg = colors.accent, guibg = colors.dark },
-      -- indicator is a vert before buffer tab
-      indicator_selected = { guifg = colors.bg, guibg = colors.bg },
-      -- separators
-      separator = { guifg = colors.dark, guibg = colors.dark },
-      separator_visible = { guifg = colors.red, guibg = colors.bg },
-      separator_selected = { guifg = colors.red, guibg = colors.bg },
-      -- modified
-      modified = { guifg = colors.accent, guibg = colors.dark },
-      modified_visible = { guifg = colors.red, guibg = colors.bg },
-      modified_selected = { guifg = colors.red, guibg = colors.bg },
-      -- close buttons
-      close_button = { guifg = colors.accent, guibg = colors.dark },
-      close_button_visible = { guifg = colors.accent, guibg = colors.bg },
-      close_button_selected = { guifg = colors.accent, guibg = colors.bg }
-    }
+    -- highlights = {
+    --   -- fill is the color of the bufferline (set to be the same as galaxyline)
+    --   fill = { guifg = colors.dark, guibg = colors.line_bg },
+    --   -- this is the default skin of a buffer tab
+    --   background = { guifg = colors.accent, guibg = colors.dark },
+    --   -- buffers
+    --   buffer_visible = { guifg = colors.accent, guibg = colors.bg },
+    --   buffer_selected = { guifg = colors.white, guibg = colors.bg, gui = "bold" },
+    --   -- tabs
+    --   tab = { guifg = colors.accent, guibg = colors.red },
+    --   tab_selected = { guifg = colors.accent, guibg = colors.bg },
+    --   -- this is a close buttom at right of bufferline (disabled)
+    --   tab_close = { guifg = colors.accent, guibg = colors.dark },
+    --   -- indicator is a vert before buffer tab
+    --   indicator_selected = { guifg = colors.bg, guibg = colors.bg },
+    --   -- separators
+    --   separator = { guifg = colors.dark, guibg = colors.dark },
+    --   separator_visible = { guifg = colors.red, guibg = colors.bg },
+    --   separator_selected = { guifg = colors.red, guibg = colors.bg },
+    --   -- modified
+    --   modified = { guifg = colors.accent, guibg = colors.dark },
+    --   modified_visible = { guifg = colors.red, guibg = colors.bg },
+    --   modified_selected = { guifg = colors.red, guibg = colors.bg },
+    --   -- close buttons
+    --   close_button = { guifg = colors.accent, guibg = colors.dark },
+    --   close_button_visible = { guifg = colors.accent, guibg = colors.bg },
+    --   close_button_selected = { guifg = colors.accent, guibg = colors.bg }
+    -- }
 }
