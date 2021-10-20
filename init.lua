ecce0c72333dba352825dd51300a50ac9b0bd843
Marcos Romero Lamas <marromlam@gmail.com>
2021-10-20 19:24:21 +0200

    prepare to handle external modules

--- init.lua
+++ init.lua
@@ -1,16 +1,16 @@
--- 
---   ,dPYb,                                                                
---   IP'`Yb                                                                
---   I8  8I                                         gg                     
---   I8  8'                                         ""                     
---   I8 dP  gg      gg    ,gggg,gg     ggg    gg    gg    ,ggg,,ggg,,ggg,  
---   I8dP   I8      8I   dP"  "Y8I    d8"Yb   88bg  88   ,8" "8P" "8P" "8, 
---   I8P    I8,    ,8I  i8'    ,8I   dP  I8   8I    88   I8   8I   8I   8I 
+--
+--   ,dPYb,
+--   IP'`Yb
+--   I8  8I                                         gg
+--   I8  8'                                         ""
+--   I8 dP  gg      gg    ,gggg,gg     ggg    gg    gg    ,ggg,,ggg,,ggg,
+--   I8dP   I8      8I   dP"  "Y8I    d8"Yb   88bg  88   ,8" "8P" "8P" "8,
+--   I8P    I8,    ,8I  i8'    ,8I   dP  I8   8I    88   I8   8I   8I   8I
 --  ,d8b,_ ,d8b,  ,d8b,,d8,   ,d8b,,dP   I8, ,8I  _,88,_,dP   8I   8I   Yb,
 --  8P'"Y888P'"Y88P"`Y8P"Y8888P"`Y88"     "Y8P"   8P""Y88P'   8I   8I   `Y8
---  
+--
 --  This is the main configuration file for the lua-based nvim configuration.
---  The main configuration flags can be controlled here, such as themes and 
+--  The main configuration flags can be controlled here, such as themes and
 --  the compulsory-to-be-loaded packages. All the rest of packages are lazy
 --  loaded upon user request.
 --
@@ -18,10 +18,38 @@
 --  Created on Aug 20 of 2021
 
 
--- Set appearance
-vim.g.colorscheme = "material"
-vim.g.colors_style = "darker"
-vim.g.colors_lighter_contrast = false
+vim.g.elite_mode = true
+
+
+-- Set appearance {{{
+
+vim.o.background = "dark" -- or "light" for light mode
+-- vim.g.colors_lighter_contrast = false
+
+-- gruvbox theme {{{
+vim.cmd([[colorscheme gruvbox]])
+vim.g.gruvbox_contrast_dark = 'hard'
+vim.g.gruvbox_sign_column = 'bg0'
+-- }}}
+
+
+-- tokyonight theme {{{
+-- vim.cmd([[colorscheme tokyonight]])
+-- }}}
+
+
+-- VScode theme {{{
+-- vim.cmd[[ colorscheme vscode ]]
+-- vim.g.vscode_style = "dark"
+-- }}}
+
+
+-- material theme {{{
+-- vim.cmd[[ colorscheme material ]]
+-- let g:material_style = 'palenight'
+-- }}}
+
+-- }}}
 
 
 -- Set binaries
@@ -41,3 +69,21 @@ vim.cmd "silent! command PackerUpdate lua require 'packer_list' require('packer'
 -- Load only stricly needed config
 require "general.settings"
 require "keys.mappings".config()
+
+vim.cmd [[
+function! Synctex()
+    let vimura_param = " --synctex-forward " . line('.') . ":" . col('.') . ":" . expand('%:p') . " " . substitute(expand('%:p'),"tex$","pdf", "")
+    if has('nvim')
+        call jobstart("vimura neovim" . vimura_param)
+    else
+        exe "silent !vimura vim" . vimura_param . "&"
+    endif
+    redraw!
+endfunction
+]]
+
+
+vim.cmd[[ let &runtimepath.=',' . expand("$HOME") . '/personal/harpoon/master' ]]
+
+
+-- vim:foldmethod=marker
