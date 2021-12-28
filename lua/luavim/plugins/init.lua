6af610483a16d9fee138010b5c659e1b9dbcd829
Marcos Romero Lamas <marromlam@gmail.com>
2021-12-28 03:23:15 +0100

    feat add startuptime function

--- lua/luavim/plugins/init.lua
+++ lua/luavim/plugins/init.lua
@@ -76,6 +76,16 @@ return packer.startup(function(use)
     end
   }
 
+  use {
+    'dstein64/vim-startuptime',
+    disable = false,
+    cmd = 'StartupTime',
+    config = function()
+      vim.g.startuptime_tries = 15
+      vim.g.startuptime_exe_args = { '+let g:auto_session_enabled = 0' }
+    end,
+  }
+
   -- theme
   use {
     "folke/tokyonight.nvim",
