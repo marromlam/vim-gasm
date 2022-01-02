a452e90d1c37587ab2265cbe55be2a34e187fdcb
Marcos Romero Lamas <marromlam@gmail.com>
2022-01-02 17:04:06 +0100

    fix(treesiter) somehow I had it disabled

--- lua/luavim/plugins/config/treesitter.lua
+++ lua/luavim/plugins/config/treesitter.lua
@@ -1,14 +1,14 @@
-local status_ok, configs = pcall(require, "nvim-treesitter.configs")
-if not status_ok then
+local present, configs = pcall(require, "nvim-treesitter.configs")
+if not present then
 	return
 end
-
 
 configs.setup {
   -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   ensure_installed = {
      "bash",
      "lua",
+     "vim",
      "cpp",
      "python",
      "latex",
@@ -17,8 +17,10 @@ configs.setup {
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
   highlight = {
+    enable = true,
     disable = { "" },  -- list of language that will be disabled
     additional_vim_regex_highlighting = true,
+    use_languagetree = true,
   },
   indent = { enable = true, disable = { "yaml" } },
   context_commentstring = {
@@ -26,3 +28,5 @@ configs.setup {
       enable_autocmd = false,
     }
 }
+
+-- vim: fdm=marker
