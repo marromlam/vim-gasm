60da1afcc74036d845eb91031092a08f46cb8e01
Marcos Romero Lamas <marcos.romero.lamas@cern.ch>
2022-06-03 17:07:53 +0000

    feat: new autopairs conf (temp)

--- lua/luavim/plugins/config/autopairs.lua
+++ lua/luavim/plugins/config/autopairs.lua
@@ -1,18 +1,53 @@
-local present, autopairs = pcall(require, "nvim-autopairs")
-if not present then
-	return
+-- local present, autopairs = pcall(require, "nvim-autopairs")
+-- if not present then
+-- 	return
+-- end
+--
+-- autopairs.setup({ check_ts = true, ts_config = {} })
+--
+-- local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+-- local present, cmp = pcall(require, "cmp")
+-- if not present then
+-- 	return
+-- end
+-- cmp.event:on('confirm_done',
+--              cmp_autopairs.on_confirm_done({  map_char = { tex = '' } })
+-- )
+
+
+local status_ok, npairs = pcall(require, "nvim-autopairs")
+if not status_ok then
+  return
 end
 
-autopairs.setup({ check_ts = true, ts_config = {} })
+npairs.setup {
+  check_ts = true,
+  ts_config = {
+    lua = { "string", "source" },
+    javascript = { "string", "template_string" },
+    java = false,
+  },
+  disable_filetype = { "TelescopePrompt", "spectre_panel" },
+  fast_wrap = {
+    map = "<M-e>",
+    chars = { "{", "[", "(", '"', "'" },
+    pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+    offset = 0, -- Offset from pattern match
+    end_key = "$",
+    keys = "qwertyuiopzxcvbnmasdfghjkl",
+    check_comma = true,
+    highlight = "PmenuSel",
+    highlight_grey = "LineNr",
+  },
+}
 
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local present, cmp = pcall(require, "cmp")
-if not present then
-	return
+local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then
+  return
 end
-cmp.event:on('confirm_done',
-             cmp_autopairs.on_confirm_done({  map_char = { tex = '' } })
-)
+cmp.event:on("confirm_done",
+             cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
 
 
--- vim:foldmethod=marker
+-- vim:fdm=marker
