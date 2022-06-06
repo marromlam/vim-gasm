a2173e6f02596dd899a200ea215fc7870ab6652a
Marcos Romero Lamas <marcos.romero.lamas@cern.ch>
2022-06-06 06:34:37 +0000

    fix: copilot giving problems

--- lua/luavim/plugins/config/cmp.lua
+++ lua/luavim/plugins/config/cmp.lua
@@ -144,7 +144,7 @@ cmp.setup({
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
-    { name = 'copilot' },
+    -- { name = 'copilot' },
     { name = 'cmp_tabnine' },
     { name = 'spell' },
     { name = 'path' },
