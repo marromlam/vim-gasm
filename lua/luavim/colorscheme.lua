38447ea20ecb64a920e98f761f611e98ce30970e
Marcos Romero Lamas <mromerol@cern.ch>
2022-09-18 13:55:30 +0200

    feat: configure gruvbox

--- lua/luavim/colorscheme.lua
+++ lua/luavim/colorscheme.lua
@@ -1,3 +1,20 @@
+local present_gruvbox, gruvbox = pcall(require, "gruvbox")
+if present_gruvbox then
+  require("gruvbox").setup {
+    undercurl = true,
+    underline = true,
+    bold = true,
+    italic = true, -- will make italic comments and special strings
+    inverse = true, -- invert background for search, diffs, statuslines and errors
+    invert_selection = true,
+    invert_signs = false,
+    invert_tabline = false,
+    invert_intend_guides = false,
+    contrast = "hard", -- can be "hard" or "soft"
+    overrides = {},
+  }
+end
+
 vim.cmd [[
 try
   colorscheme gruvbox
