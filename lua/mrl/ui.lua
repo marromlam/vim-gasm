dcf7328b8e4b4d6eee6e04f4216d3454fd6f3b9b
Marcos Romero Lamas <marromlam@gmail.com>
2023-11-03 21:22:06 +0100

    feat: wider block for git diff

--- lua/mrl/ui.lua
+++ lua/mrl/ui.lua
@@ -34,8 +34,8 @@ mrl.ui.icons = {
     right_thin_block = "▕",
     vert_bottom_half_block = "▄",
     vert_top_half_block = "▀",
-    -- right_block = "🮉",
-    right_block = "▕",
+    right_block = "🮉",
+    -- right_block = "▕",
     light_shade_block = "░",
   },
   lsp = {
