f51b2d971d7cebb999b188f7a07ac5777d1e9ce9
Marcos Romero Lamas <marromlam@gmail.com>
2023-11-06 10:21:58 +0100

    fix: unfocused pane color

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,7 @@
 function ColorMyPencils(color)
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 end
 
 ColorMyPencils()
